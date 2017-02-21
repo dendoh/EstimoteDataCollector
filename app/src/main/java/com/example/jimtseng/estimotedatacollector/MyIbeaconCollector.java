e55d667d38656669b2b1e4f00e36184e440e16d9
jimtseng <lamuness@gmail.com>
2017-02-22 00:31:56 +0800

    Initial commit - Layout created

---
+++ app/src/main/java/com/example/jimtseng/estimotedatacollector/MyIbeaconCollector.java
@@ -0,0 +1,13 @@
+package com.example.jimtseng.estimotedatacollector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyIbeaconCollector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_ibeacon_collector);
+    }
+}
