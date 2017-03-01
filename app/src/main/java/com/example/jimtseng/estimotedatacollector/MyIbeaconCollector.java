848757aee7101757d545e2de0b2b23f6af2e7863
jimtseng <lamuness@gmail.com>
2017-03-02 01:56:14 +0800

    Basic working code   -drop down selection implemented   -BLE scanner is functional

--- app/src/main/java/com/example/jimtseng/estimotedatacollector/MyIbeaconCollector.java
+++ app/src/main/java/com/example/jimtseng/estimotedatacollector/MyIbeaconCollector.java
@@ -1,13 +1,173 @@
 package com.example.jimtseng.estimotedatacollector;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.util.Log;
+import android.util.TimeUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.StringBuilder;
+import java.lang.String;
 
 public class MyIbeaconCollector extends AppCompatActivity {
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mLEScanner;
+    private ScanSettings settings;
+    private List<ScanFilter> filters;
+    private BluetoothGatt mGatt;
+    private Handler mHandler;
+    private ScanResult SharedScanResult;
+    final String TAG = "EstimoteCollector";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_ibeacon_collector);
+        Spinner myspinner = (Spinner) findViewById(R.id.spinner_cell);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cell,
+                android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(adapter);
+        myspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,"position:"+position+" is selected");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        mHandler = new Handler();
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        Log.d(TAG, "getting BT adapter....");
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
+        filters = new ArrayList<ScanFilter>();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart...");
+      //  scanLeDevice(true);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume...");
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "BT is not enabled, requesting to enable");
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+        scanLeDevice(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mBluetoothAdapter!=null&&mBluetoothAdapter.isEnabled()) {
+            scanLeDevice(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 1 && resultCode == Activity.RESULT_CANCELED) {
+            Log.d(TAG, "BT cannot be enabled");
+            finish();
+            return;
+        }
+    }
+    private void scanLeDevice(boolean enable) {
+        Log.d(TAG, "scanLeDevice enable="+enable);
+        if(enable){
+            Log.d(TAG, "Starting scan");
+            mLEScanner.startScan(filters,settings,mLEScanCallback_new);
+            // mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            Log.d(TAG, "Stopping scan");
+            mLEScanner.stopScan(mLEScanCallback_new);
+            // mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    private ScanCallback mLEScanCallback_new = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            SharedScanResult = result;
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    String payload_for_log = String.format(
+                            "[TimeTAG]" + SharedScanResult.getTimestampNanos()
+                                    +" BLEDevice:" + SharedScanResult.getDevice().toString()
+                                    + " Record:" + bytesToHex(SharedScanResult.getScanRecord().getBytes())
+                                    + " rssi:" + SharedScanResult.getRssi()
+                                    + "\n");
+                    String payload_for_mqtt = String.format(
+                            SharedScanResult.getTimestampNanos()
+                                    +"," + SharedScanResult.getDevice().toString()
+                                    + "," + bytesToHex(SharedScanResult.getScanRecord().getBytes())
+                                    + "," + SharedScanResult.getRssi());
+                }
+            });
+              Log.d(TAG, "BLEDevice:" + result.getDevice().toString() + " Record:" + bytesToHex(result.getScanRecord().getBytes()) + " rssi:" + result.getRssi());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult sr : results) {
+                Log.d(TAG, "[Batch]BLEDevice:" + sr.getDevice().toString() + " Record:" + bytesToHex(sr.getScanRecord().getBytes()) + " rssi:" + sr.getRssi());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e(TAG,"Scan error code:"+errorCode);
+        }
+    };
+
+    private static String bytesToHex(byte[] in) {
+        final StringBuilder builder = new StringBuilder();
+        for(byte b : in) {
+            builder.append(String.format("0x%02x-", b));
+        }
+        return builder.toString();
     }
 }
